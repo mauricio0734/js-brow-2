591887f9101182866eed03657a101d47f60736a1
Mauricio Barón <diegombaron@gmail.com>
2023-06-01 22:24:05 -0500

    Final ejercicio

--- index.js
+++ index.js
@@ -1,43 +1,73 @@
-let task = [];
+let tasks = [];
 let id = 1;
 
-const addButton = document.getElementById('addButton');
-const listItems = document.getElementById('listItems');
+const addButton = document.getElementById("addButton");
+const deleteButton = document.getElementById("deleteButton");
+const listItems = document.getElementById("listItems");
 
-function renderTask(task){
-    const li = document.createElement('li');
-   const input = document.createElement('input');
-   input.setAttribute('type', 'checkbox');
-   const span = document.createElement('span');
-   span.textContent = task.title;
+function renderTask(task) {
+  const li = document.createElement("li");
+  const input = document.createElement("input");
+  input.setAttribute("type", "checkbox");
+  const span = document.createElement("span");
+  span.textContent = task.title;
+  span.textContent = task.title;
 
-li.appendChild(input);
-li.appendChild(span);
-li.addEventListener('click', function(event, task){
+  li.appendChild(input);
+  li.appendChild(span);
+  li.addEventListener("click", function (event, task) {
     handleCheckTask(event, task);
-});
-listItems.appendChild(li);
+  });
+  listItems.appendChild(li);
 }
 
-
-function handleSubmit(event){
-event.preventDefault();
-const input = document.getElementById('tarea');
-const task = {
+function handleSubmit(event) {
+  event.preventDefault();
+  const input = document.getElementById("tarea");
+  const task = {
     id,
     title: input.value,
-    complete: false
+    complete: false,
+  };
+  tasks.push(task);
+  id += 1;
+  input.value = "";
+  renderTask(task);
 }
-tasks.push(task);
-id += 1;
-input.value ='';
-renderTask(task);
+
+function handleCheckTask(event) {
+  const input = event.currentTarget.querySelector("input");
+  const span = event.currentTarget.querySelector("span");
+  const isChecked = input.getAttribute("checked");
+
+  if (isChecked) {
+    input.removeAttribute("checked");
+    span.classList.remove("is-completed");
+    tasks = tasks.map((item) =>
+      item.id === task.id ? { ...item, complete: false } : item
+    );
+  } else {
+    input.setAttribute("checked", true);
+    span.classList.add("is-completed");
+    tasks = tasks.map((item) =>
+      item.id === task.id ? { ...item, complete: true } : item
+    );
+  }
 }
 
-function handleCheckTask(event){}
+function handleDelete(event) {
+  event.preventDefault();
+  const list = listItems.querySelectorAll("li");
+  list.forEach((element) => {
+    const isChecked = element.children[0].getAttribute("checked");
+    isChecked ? element.remove() : null;
+  });
+}
 
-function handleDelete(event){}
-
-addButton.addEventListener('clic' , function(event){
-    handleSubmit(event);
-})
+addButton.addEventListener("click", function (event) {
+  console.log(this);
+  handleSubmit(event);
+});
+deleteButton.addEventListener("click", function (event) {
+  handleDelete(event);
+});
